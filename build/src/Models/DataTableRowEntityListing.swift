9d3353aeb249266f7c8396434bed6b57da67d659
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2020-06-10 06:30:10 +0000

    29.0.0

--- build/src/Models/DataTableRowEntityListing.swift
+++ build/src/Models/DataTableRowEntityListing.swift
@@ -18,11 +18,11 @@ public class DataTableRowEntityListing: Codable {
     public var firstUri: String?
     public var selfUri: String?
     public var nextUri: String?
-    public var lastUri: String?
     public var previousUri: String?
+    public var lastUri: String?
     public var pageCount: Int?
 
-    public init(entities: [[String:JSON]]?, pageSize: Int?, pageNumber: Int?, total: Int64?, firstUri: String?, selfUri: String?, nextUri: String?, lastUri: String?, previousUri: String?, pageCount: Int?) {
+    public init(entities: [[String:JSON]]?, pageSize: Int?, pageNumber: Int?, total: Int64?, firstUri: String?, selfUri: String?, nextUri: String?, previousUri: String?, lastUri: String?, pageCount: Int?) {
         
         self.entities = entities
         
@@ -38,9 +38,9 @@ public class DataTableRowEntityListing: Codable {
         
         self.nextUri = nextUri
         
-        self.lastUri = lastUri
-        
         self.previousUri = previousUri
+        
+        self.lastUri = lastUri
         
         self.pageCount = pageCount
         

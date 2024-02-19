4d4adf2dd228db29f9e34c8baca3c78838d4714c
Praveen Bhaskar <p_bhaskar@apple.com>
2024-02-19 11:52:58 +0530

    Update path and typealias

---
+++ MyPureCloud/Sources/Libraries/GenericJSON/Querying.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+public extension JSON {
+
+    /// Return the string value if this is a `.string`, otherwise `nil`
+    var stringValue: String? {
+        if case .string(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    /// Return the float value if this is a `.number`, otherwise `nil`
+    var floatValue: Float? {
+        if case .number(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    /// Return the bool value if this is a `.bool`, otherwise `nil`
+    var boolValue: Bool? {
+        if case .bool(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    /// Return the object value if this is an `.object`, otherwise `nil`
+    var objectValue: [String: JSON]? {
+        if case .object(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    /// Return the array value if this is an `.array`, otherwise `nil`
+    var arrayValue: [JSON]? {
+        if case .array(let value) = self {
+            return value
+        }
+        return nil
+    }
+
+    /// Return `true` iff this is `.null`
+    var isNull: Bool {
+        if case .null = self {
+            return true
+        }
+        return false
+    }
+
+    /// If this is an `.array`, return item at index
+    ///
+    /// If this is not an `.array` or the index is out of bounds, returns `nil`.
+    subscript(index: Int) -> JSON? {
+        if case .array(let arr) = self, arr.indices.contains(index) {
+            return arr[index]
+        }
+        return nil
+    }
+
+    /// If this is an `.object`, return item at key
+    subscript(key: String) -> JSON? {
+        if case .object(let dict) = self {
+            return dict[key]
+        }
+        return nil
+    }
+}
